e52d30870d3bf34aed4452f39a2c0825d33552a2
Matthew Jones <matnjones@gmail.com>
2025-11-04 14:31:01 +0000

    Updates

--- assets/html2pdf-DYS6OcbK.js
+++ assets/html2pdf-DYS6OcbK.js
@@ -1,4 +1,4 @@
-import{g as Nu,j as Gu,c as Rs,a as Vu}from"./index-8OcGH_UL.js";var Yt={exports:{}};const Xu=Nu(Gu);var lt={exports:{}};/*!
+import{g as Nu,j as Gu,c as Rs,a as Vu}from"./index-CBLlfIHw.js";var Yt={exports:{}};const Xu=Nu(Gu);var lt={exports:{}};/*!
  * html2canvas 1.4.1 <https://html2canvas.hertzen.com>
  * Copyright (c) 2022 Niklas von Hertzen <https://hertzen.com>
  * Released under MIT License
